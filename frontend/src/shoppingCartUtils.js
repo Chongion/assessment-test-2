4c8035b2325b9d83fb81a10956980e1be5c780b8
Chongion <eveliina.c.laakkonen@gmail.com>
2022-10-09 21:37:05 +0300

    fixed the code

--- frontend/src/shoppingCartUtils.js
+++ frontend/src/shoppingCartUtils.js
@@ -1,15 +1,28 @@
 // Takes a list of fruit objects as arguments
 // eg. [{'name': 'Banana', 'price': 1}, {'name': 'Apple', 'price': 2}]
 // Should return the cart total.
-function calculateTotal (items) {
-  return 9999;
+function calculateTotal(items) {
+  let total = 0;
+  for (let i = 0; i < items.lenght; i++) {
+    return total
+  }
 }
 
 // Takes a list of fruit objects as arguments
 // eg. [{'name': 'Banana', 'price': 1}, {'name': 'Apple', 'price': 2}]
 // Should return the cart average.
-function calculateAvg (items) {
-  return 9999;
+function calculateAvg(items) {
+  let average = 0;
+
+  let total = 0;
+
+  for (let i = 0; i < items.lenght; i++) {
+    total += items[i].price;
+
+    average = total / items.lenght;
+  }
+  return average
 }
 
 export { calculateTotal, calculateAvg };
+
